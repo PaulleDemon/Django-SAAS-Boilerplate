fe2f5cf0cfc3e904077f1c9d67515b7083fce2e9
paul <paul.sajo.official@gmail.com>
2024-09-03 13:58:36 +0530

    update: display created time on admin transaction

--- transaction/admin.py
+++ transaction/admin.py
@@ -24,11 +24,13 @@ class PlanAdmin(ModelAdmin):
 @admin.register(Transaction)
 class TransactionAdmin(ModelAdmin):
 
-    list_display = ['id', 'transaction_id', 'user', 'plan', 'status', 'total']
+    list_display = ['id', 'transaction_id', 'user', 'plan', 'status', 'created', 'total']
 
     search_fields = ['transaction_id', 'user__email']
 
     list_filter = ['plan', 'status', 'created']
+
+    readonly_fields = ['created', 'modified']
 
     # def get_total(self, obj):
     #     return f'${obj.get_total_dollars()}'
